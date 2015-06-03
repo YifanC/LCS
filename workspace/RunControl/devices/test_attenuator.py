b0e3a45933c459c83763f06785e1fb2db8493076
laser <laser@uboonelaserone.fnal.gov>
2015-06-03 11:39:16 -0500

    nothing important

--- workspace/RunControl/devices/test_attenuator.py
+++ workspace/RunControl/devices/test_attenuator.py
@@ -3,17 +3,20 @@ __author__ = 'matthias'
 from devices.attenuator import *
 
 att = Attenuator()
+att.color = False
 att.comTimeout = 2
 
 att.comport = "/dev/ttyUSB9"
-#att.com_init()
-att.color = False
+att.com_init()
+
 
 
 att.config.microstep_resolution = 3.0
 
-att.setZero(123.)
+att.setZero(444.)
+att.setZero(423.)
 
+#att.com_close()
 #
 # att.getParameter("acceleration")
 #att.getInfo(True)
