be4219ccc04080509924c83d18b1347d4801e02a
matthias <matthias.luethi@lhep.unibe.ch>
2015-07-16 17:08:25 -0500

    filled functions for szstems

--- workspace/RunControl/base/controls.py
+++ workspace/RunControl/base/controls.py
@@ -21,57 +21,63 @@ class Controls(base):
             self.printError("Could not find path to base files -> aborting")
             sys.exit(1)
 
-    def test(self, string):
-        print string
-
-    def encoder_start(RunNr):
-        ProdEncoder = subprocess.Popen(['./write.o'])
-        return ProdEncoder
+    def encoder_start(self):
+        self.printMsg("Starting Encoder")
+        self.proc_encoder = self.process_start(self.path_services + "/" + "test_client.o", c=True)
 
     def encoder_alive(self):
-        pass
+        alive = self.process_alive(self.proc_encoder)
+        self.printMsg("Is Encoder alive? " + str(alive))
+        return alive
 
     def encoder_stop(self):
-        #subEncoder.send_signal(signal.SIGINT)
-        pass
+        self.printMsg("Stopping Encoder")
+        self.process_stop(self.proc_encoder)
 
     def assembler_start(self):
         self.printMsg("Starting Assembler")
         self.proc_assembler = self.process_start(self.path_services + "/" + "test_server.py", py=True)
 
     def assembler_alive(self):
-        return self.process_alive(self.proc_assembler)
+        alive = self.process_alive(self.proc_assembler)
+        self.printMsg("Is Assembler alive? " + str(alive))
+        return alive
 
     def assembler_stop(self):
         self.printMsg("Stopping Assembler")
         self.process_stop(self.proc_assembler)
 
     def broker_start(self):
-
+        self.printMsg("Starting Broker")
+        self.proc_broker = self.process_start(self.path_services + "/" + "test_broker.py", py=True)
         pass
 
     def broker_alive(self):
-        pass
+        alive = self.process_alive(self.proc_broker)
+        self.printMsg("Is Broker alive? " + str(alive))
+        return alive
 
     def broker_stop(self):
-        pass
+        self.printMsg("Stopping Broker")
+        self.process_stop(self.proc_broker)
 
     def process_start(self, filename, args="", py=False, c=False):
         if py is True:
             prefix = "python"
             command =  [prefix, filename, args]
         elif c is True:
-            prefix = "./"
-            command =  [prefix + command, args]
+            #prefix = "./"
+            command =  [filename, args]
         else:
             self.printError("do not know how to execute!")
             return False
-
-        # For debugging process = subprocess.Popen(command)
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        # For debugging
+        #process = subprocess.Popen(command)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=False)
         return process
 
     def process_alive(self, process):
+        # TODO: Check if process exists!
         if process.poll() is None:
             return True
         else:
@@ -87,9 +93,9 @@ class Controls(base):
 
     def test(self):
         print "starting python process"
-        proc1 = self.process_start("test_proc.py", py=True, args="-p 1")
+        proc1 = self.process_start(self.path_base + "/" + "test_proc.py", py=True, args="-p 1")
         print "starting c process"
-        proc2 = self.process_start("./test_proc.o", c=True)
+        proc2 = self.process_start(self.path_base + "/" + "test_proc.o", c=True)
         #print proc2
         time.sleep(2)
         print "is the python process alive? \n" + str(self.process_alive(proc1))
@@ -99,3 +105,5 @@ class Controls(base):
         time.sleep(1)
         print "is the python process alive? \n" + str(self.process_alive(proc1))
         print "is the c process alive? \n" + str(self.process_alive(proc2))
+
+        print "closing a non existent process"
