fa3de330f495d2a9c8080cb654bbc3bc12a78d1f
matthias <matthias.luethi@lhep.unibe.ch>
2015-07-21 14:29:37 -0500

    implemented read and write Register functions

--- workspace/RunControl/devices/mirror.py
+++ workspace/RunControl/devices/mirror.py
@@ -18,13 +18,15 @@ class Mirror(Motor):
         self.InfoMsgLength = 100
         self.StandartMsgLength = 15
         # config file
-        #self.config_setfile()
+        # self.config_setfile()
         #self.config_load()
 
         self.InstructionSet = {"getName": 50,
                                "stopMovement": 23,
                                "home": 1,
                                "status": 54,
+                               "readRegister": 35,
+                               "setRegister": 35,
                                "getPosition": 60,
                                "moveAbsolute": 20,  # no absolute movement implemented in hardware
                                "moveRelative": 21}  # positive direction closes / negative opens
@@ -35,19 +37,26 @@ class Mirror(Motor):
 
         self.axis = 1
 
-    def communicate(self, command, data=0):
+    def com_send(self, command, instruction=0):
+        if instruction == 0:
+            instruction = [0, 0, 0, 0]
+        else:
+            if len(instruction) != 4:
+                self.printError("data frame has wrong size")
 
-        msg = struct.pack("<BBl", self.axis, command, data)
+        msg = struct.pack("<" + 6*"B", self.axis, command, instruction[0], instruction[1], instruction[2], instruction[3])
         reply = self.com_write(msg, echo=True)
         self.translate_reply(reply)
-	self.printMsg(reply)
+        self.printMsg(reply)
+
+        return reply
 
     def translate_reply(self, reply):
-        r = [0,0,0,0,0,0]
-        for i in range (6):
+        r = [0, 0, 0, 0, 0, 0]
+        for i in range(6):
             r[i] = ord(reply[i])
 
-        self.printMsg(r)
+        self.printDebug(r)
         return r
 
     def setParameter(self, parameter):
@@ -56,9 +65,40 @@ class Mirror(Motor):
     def getParameter(self, parameter, value):
         pass
 
-    def readSerialNumber(self):
+    def setSerial(self,serial):
+        address = 0
+        self.writeRegister(address, serial)
+        return 0
 
-        pass
+    def getSerial(self):
+        address = 0
+        return self.readRegister(address)
+
+    def readRegister(self, register_adr):
+        if 0 > register_adr > 127:
+            self.printError("Address out of bounds")
+            return -1
+        instruction = [register_adr, 0, 0, 0]
+        reply = self.com_send(self.InstructionSet["readRegister"], instruction)
+        return reply
+
+    def writeRegister(self, register_adr, value):
+        if 0 > register_adr > 127:
+            self.printError("Address out of bounds")
+            return -1
+
+        if type(value) is not int:
+            self.printError("value has wrong format")
+            return -1
+
+        if 255 > value > 0:
+            self.printError("value is out of bounds")
+            return -1
+
+        instruction = [register_adr | 128, value[0], 0, 0]
+        self.com_send(self.InstructionSet["readRegister"], instruction)
+        return 0
+
     def moveAbsolute(self, value, monitor=False, display=False, delta=10):
         pass
 
