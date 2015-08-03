350897bff1caf2813687d42bbf95e116bb6a3293
laser <laser@uboonelaserone.fnal.gov>
2015-08-03 16:35:07 -0500

    fixed communication with laser

--- workspace/RunControl/devices/test_laser.py
+++ workspace/RunControl/devices/test_laser.py
@@ -3,13 +3,13 @@ __author__ = 'matthias'
 from devices.laser import *
 
 laser = Laser()
-laser.comTimeout = 2
-laser.comport = "/dev/pts/10"
+laser.comTimeout = 0.5
+laser.comport = "/dev/ttyUSB3"
 
 laser.com_init()
 #
 # att.getParameter("acceleration")
-#att.getInfo(True)
+laser.getStatus()
 
 laser.getShots()
 laser.com_close()
