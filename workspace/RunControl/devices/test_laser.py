6126afc6b031ed266db1a014e1ed39a94363593f
laser <laser@uboonelasertwo.fnal.gov>
2015-08-04 15:47:41 -0500

    changed position of laser

--- workspace/RunControl/devices/test_laser.py
+++ workspace/RunControl/devices/test_laser.py
@@ -4,11 +4,10 @@ from devices.laser import *
 
 laser = Laser()
 laser.comTimeout = 0.5
-laser.comport = "/dev/ttyUSB3"
+laser.comport = "/dev/ttyUSB4"
 
 laser.com_init()
-#
-# att.getParameter("acceleration")
+
 laser.getStatus()
 
 laser.getShots()
