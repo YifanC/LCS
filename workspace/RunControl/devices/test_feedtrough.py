9b2bd1741cf13beecde0c77299727f16682d8ccd
laser <laser@uboonelaserone.fnal.gov>
2015-08-06 14:33:24 -0500

    added init axis

--- workspace/RunControl/devices/test_feedtrough.py
+++ workspace/RunControl/devices/test_feedtrough.py
@@ -4,14 +4,17 @@ import serial
 from devices.feedtrough import *
 
 
-LinearMotor = Feedtrough("linear_actuator", 1)
-RotaryMotor = Feedtrough("rotary_actuator", 2)
+LinearMotor = Feedtrough("linear_actuator", 2)
+RotaryMotor = Feedtrough("rotary_actuator", 1)
 LinearMotor.color = False
 LinearMotor.comport = "/dev/ttyUSB0"
 LinearMotor.com_init()
 RotaryMotor.com = LinearMotor.com
 LinearMotor.color = False
 RotaryMotor.color = False
+
+RotaryMotor.initAxis()
+LinearMotor.initAxis()
 
 RotaryMotor.getInfo(True)
 LinearMotor.getInfo(True)
