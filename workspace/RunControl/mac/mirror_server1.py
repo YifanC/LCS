e095d28da1bfc9b69e6a705969352ce46e92710b
laser <laser@uboonelaserone.fnal.gov>
2015-08-11 21:17:35 -0500

    fiexed som errors

--- workspace/RunControl/mac/mirror_server1.py
+++ workspace/RunControl/mac/mirror_server1.py
@@ -5,7 +5,8 @@ mirror111 = Mirror("mirror111", 1)
 mirror112 = Mirror("mirror112", 2)
 mirror111.com_init()
 mirror112.com = mirror111.com
-
+mirror111.color = False
+mirror112.color = False
 mirror111.getSerial()
 mirror112.getSerial()
 
@@ -13,9 +14,10 @@ mirror112.getSerial()
 mirror121 = Mirror("mirror121", 1)
 mirror122 = Mirror("mirror122", 2)
 mirror121.com_init()
-mirror122.com = mirror111.com
-
+mirror122.com = mirror121.com
+mirror122.color = False
+mirror121.color = False
 mirror121.getSerial()
-mirror12a2.getSerial()
+mirror122.getSerial()
 # pretend mirror221 is vertical / mirror222 is horizontal
 
