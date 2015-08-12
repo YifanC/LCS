15348f92021b9f0f9a7020ce9f1c804f90c99bc4
matthias <matthias.luethi@lhep.unibe.ch>
2015-08-12 11:04:06 -0500

    updated and added mirror config for laser 1 and 2

--- workspace/RunControl/devices/test_laser.py
+++ workspace/RunControl/devices/test_laser.py
@@ -4,11 +4,12 @@ from devices.laser import *
 
 laser = Laser()
 laser.comTimeout = 0.5
+laser.comDryRun = True
 laser.comport = "/dev/ttyUSB4"
 
 laser.com_init()
 
 laser.getStatus()
-
+laser.setRate(1)
 laser.getShots()
 laser.com_close()
