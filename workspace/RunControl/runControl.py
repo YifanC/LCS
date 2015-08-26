a0ce852fba5eac63e72b245d63531b1483228125
matthias <matthias.luethi@lhep.unibe.ch>
2015-08-26 16:23:55 -0500

    Serveral developments: 1. added init time for laser 2. small improvements in the procedure

--- workspace/RunControl/runControl.py
+++ workspace/RunControl/runControl.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime
+
 from base.controls import *
 from services.communication import Producer
 from services.data import LaserData
@@ -16,44 +18,71 @@ from devices.mirror import *
 RunNumber = 99
 # Construct needed instances
 rc = Controls(RunNumber=RunNumber)
-data = LaserData()
+data = LaserData(RunNumber=RunNumber)
 rc.com = Producer("runcontrol")
-rc.ft_linear = Feedtrough("ft_linear", 1)
-rc.ft_rotary = Feedtrough("ft_rotary", 1)
+rc.ft_linear = Feedtrough("linear_actuator")
+rc.ft_rotary = Feedtrough("rotary_actuator")
 rc.laser = Laser()
 rc.attenuator = Attenuator()
 rc.aperture = Aperture()
-rc.mirror_x = Mirror("mirror221")  # not yet defined correctly
-rc.mirror_y = Mirror("mirror222")  # not yet defined correctly
+rc.mirror_x = Mirror("mirror221", 1)  # not yet defined correctly
+rc.mirror_y = Mirror("mirror222", 2)  # not yet defined correctly
 
 # Start broker / encoder
 rc.broker_start()
+print rc.RunNumber
 rc.assembler_start()
 time.sleep(2)
-rc.encoder_start()
 
-# Initialize and setup communication
-rc.com.id = 1
-rc.com.state = -1
-rc.com.start()
-rc.com.send_hello()
-rc.com.state = 0
+# Dry run configuration
+rc.encoder_start(dry=True)
+rc.laser.comDryRun = True
+rc.ft_rotary.comDryRun = True
+rc.ft_linear.comDryRun = True
 
 # Initialize com ports
-#rc.ft_linear.com_init()
-#rc.ft_rotary.com = rc.ft_linear.com
-#rc.laser.com_init()
+rc.ft_linear.com_init()
+rc.ft_rotary.com = rc.ft_linear.com
+rc.laser.com_init()
 #rc.attenuator.com_init()
 #rc.aperture.com_init()
 #rc.mirror_x.com_init()
 #rc.mirror_y.com = rc.mirror_x.com
 
+rc.laser.timeout = 20  # minutes
+starttime = datetime.today()
+
+initialized = False
+rc.laser.start()
+while (datetime.today() - starttime).seconds < 5: # int(60 * rc.laser.timeout):
+    rc.laser.printMsg("waiting for laser to warm up (20 minutes)")
+    if initialized is False:
+        # Initialize and setup communication
+        rc.com.id = 1
+        rc.com.state = -1
+        rc.com.start()
+        rc.com.send_hello()
+        rc.com.state = 0
+
+        # Homing Feedtrough
+        rc.ft_linear.homeAxis()
+        rc.ft_rotary.homeAxis()
+
+        # homing Attenuator
+        initialized = True
+
+    time.sleep(1)
+
+
+raw_input("Start Laser Scan?")
 # ----------------------------------------------------
 # --------------------- Operation --------------------
 # ----------------------------------------------------
 rc.com.send_data(data)
 
-time.sleep(10)
+time.sleep(5)
+rc.laser.setRate(1)
+rc.laser.openShutter()
 rc.assembler_alive()
 rc.broker_alive()
 rc.encoder_alive()
@@ -62,10 +91,14 @@ rc.encoder_alive()
 # -------------------- Finalize ----------------------
 # ----------------------------------------------------
 
+
+rc.laser.closeShutter()
+rc.laser.setRate(0)
+rc.laser.stop()
 # Close com ports
 #rc.ft_linear.com_close()
 #rc.ft_rotary.com_close()
-#rc.laser.com_close()
+rc.laser.com_close()
 #rc.attenuator.com_close()
 #rc.aperture.com_close()
 
