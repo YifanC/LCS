428331bce1954fc86e93a8f521832fd01ef1294f
laser <laser@uboonelasertwo.fnal.gov>
2015-09-27 16:14:17 -0500

    put in the mirrors in the config script

--- workspace/RunControl/mac/mirror_server2.py
+++ workspace/RunControl/mac/mirror_server2.py
@@ -15,6 +15,12 @@ mirror222 = Mirror("mirror222", 2)
 mirror221.com_init()
 mirror222.com = mirror221.com
 
+# Mirror in Laserbox
+mirror211 = Mirror("mirror211", 1)
+mirror212 = Mirror("mirror212", 2)
+mirror211.com_init()
+mirror212.com = mirror211.com
+
 mirror221.getSerial()
 mirror222.getSerial()
 # pretend mirror221 is vertical / mirror222 is horizontal
