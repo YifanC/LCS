69542bfc4d1d1e2901a3aaac613ef0bb3b717d7d
matthias <matthias.luethi@lhep.unibe.ch>
2016-06-10 14:54:10 +0200

    minor changes

--- workspace/RunControl/tools/read.py
+++ workspace/RunControl/tools/read.py
@@ -9,9 +9,9 @@ import plotly.plotly as py
 # (*) Useful Python/Plotly tools
 import plotly.tools as tls
 
-RunNumber = 3165
+RunNumber = 3166
 
-data = pandas.read_csv("../data/Run-" + str(3165) + ".txt", delim_whitespace=True)
+data = pandas.read_csv("../data/Run-" + str(RunNumber) + ".txt", delim_whitespace=True)
 
 id = int(data.iloc[1,0] -1)
 
@@ -23,15 +23,17 @@ lin_tick = 0.00001  # conversion from tick to mm
 # these offsets are defined in the laser coordinate system: origin in middle of mirror, x- axis parallel to wire plane,
 # y-axis along drift and z-axis parallel to field cage tubes
 
-lin_offset = [0, - 266.60208631 + 60]  # offsets for both systems
-rot_offset = [0, + 18 - 180]
+lin_offset = [0, - 266.60208631 + 60 + 1.161]  # offsets for both systems
+rot_offset = [0, 0 + 18 - 180]
 power_calib = [[0,0], [8000, 12500]] # first: power minimum, second: power maximum
 
 
 # reading data
 i = data.iloc[:,8]
-thetha = data.iloc[:, 3] * lin_tick / lin_convert + lin_offset[id]    # azimuthal angle
-phi = data.iloc[:, 2]  + rot_offset[id]                                                 # polar angle
+
+thetha = data.iloc[:, 3] * lin_tick / lin_convert + lin_offset[id]    # polar angle
+phi = data.iloc[:, 2]  + rot_offset[id]                                            # azimuthal angle
+
 power = (data.iloc[:, 4] - power_calib[id][0]) / (power_calib[id][1] - power_calib[id][0]) * 100
 time = data.iloc[:, 6] + data.iloc[:, 7] / 1000000 - data.iloc[0, 6]
 
@@ -40,9 +42,9 @@ time = data.iloc[:, 6] + data.iloc[:, 7] / 1000000 - data.iloc[0, 6]
 f, axarr = plt.subplots(3, sharex=True)
 
 # Plots
-axarr[0].plot(i, thetha)
-axarr[1].plot(i, phi)
-axarr[2].plot(i, power)
+axarr[0].plot(i, thetha, "*")
+axarr[1].plot(i, phi, "*")
+axarr[2].plot(i, power, "*")
 
 # Axis settings
 axarr[0].set_ylabel("Polar Angle [deg]")
