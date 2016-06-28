0a51e835e30a10d9585a03f35f9faf5056b1d4d6
matthias <matthias.luethi@lhep.unibe.ch>
2016-06-28 18:18:39 +0200

    added parser and printing of time used for scan

--- workspace/RunControl/services/test_positions.py
+++ workspace/RunControl/services/test_positions.py
@@ -1,9 +1,18 @@
 __author__ = 'matthias'
 
+import argparse
 from services.positions import *
 
-pos = Positions(123)
-pos.load('./scans/config_scan.csv')
+# Ask for the arguments
+parser = argparse.ArgumentParser(description='Test script for the defined scans')
+
+parser.add_argument("-c", "--configfile", action='store', dest='file', required=True,
+                    help='configuration file to inspect', type=str)
+
+arguments = parser.parse_args()
+
+pos = Positions(-99)
+pos.load(arguments.file)
 
 pos.plotMovement()
 
